74f0f6afe678d0cceb1da60deda89fa129319c19
Will Allen <will@wallen.me>
2025-05-20 01:29:57 -0500

    Cairo native build working

--- manim/svg-anim-compiler/main.c
+++ manim/svg-anim-compiler/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cairo.h>
 
 /**
  * [CTXT]
@@ -222,5 +223,19 @@ int main(int argc, char **argv) {
   }
 
   fclose(fp);
-  return 0;
+        cairo_surface_t *surface =
+            cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 240, 80);
+        cairo_t *cr =
+            cairo_create (surface);
+
+        cairo_select_font_face (cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+        cairo_set_font_size (cr, 32.0);
+        cairo_set_source_rgb (cr, 0.0, 0.0, 1.0);
+        cairo_move_to (cr, 10.0, 50.0);
+        cairo_show_text (cr, "Hello, world");
+
+        cairo_destroy (cr);
+        cairo_surface_write_to_png (surface, "E:\\APP\\website-content\\manim\\newtons-fractal\\hello.png");
+        cairo_surface_destroy (surface);
+        return 0;
 }
